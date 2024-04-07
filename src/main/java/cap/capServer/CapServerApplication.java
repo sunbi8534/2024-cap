4c944ee0c885f3cad9e96e4d2e11704cc77fba23
sunbi8534 <sunbi8534@github.com>
2024-04-07 18:42:01 +0900

    first commit

---
+++ src/main/java/cap/capServer/CapServerApplication.java
@@ -0,0 +1,13 @@
+package cap.capServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapServerApplication.class, args);
+	}
+
+}

9eb3b152f779ee0b3439082898745abdb98ad6df
sunbi8534 <sunbi8534@github.com>
2024-05-07 01:11:51 +0900

    swagger반영

--- src/main/java/cap/capServer/Controller/LoginController.java
+++ src/main/java/cap/capServer/Controller/LoginController.java
@@ -4,6 +4,8 @@ import cap.capServer.Dto.UserEnrollDto;
 import cap.capServer.Service.LoginService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,10 @@ public class LoginController {
         this.loginService = loginService;
     }
 
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공하면 닉네임 반환, 실패하면 false 문자열 반환")
+    })
     @Operation(summary = "로그인 API", description = "id와 pw를 받아 로그인을 시도합니다.")
     @GetMapping("/user/login")
     public String login(
@@ -26,18 +32,33 @@ public class LoginController {
         return loginService.checkUser(id, pw);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "중복이 없으면 ok 문자열 반환, 실패하면 false 문자열 반환")
+    })
+    @Operation(summary = "중복 닉네임 체크 API", description = "nickname을 받아서 기존 닉네임 중에 있는 닉네임인지 확인합니다.")
     @GetMapping("/user/dupNickname")
-    public String checkDupNickname(@RequestParam String nickname) {
+    public String checkDupNickname(
+            @Parameter(description = "사용자 nickname", required = true, example = "sunbi")
+            @RequestParam String nickname) {
         return loginService.checkDupNickname(nickname);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "중복이 없으면 ok 문자열 반환, 실패하면 false 문자열 반환")
+    })
+    @Operation(summary = "중복 ID 체크 API", description = "id를 받아서 기존 id 중에 있는 id인지 확인합니다.")
     @GetMapping("/user/dupId")
-    public String checkDupId(@RequestParam String id) {
+    public String checkDupId(
+            @Parameter(description = "사용자 id", required = true, example = "sunbi8534")
+            @RequestParam String id) {
         return loginService.checkDupId(id);
     }
 
+    @Operation(summary = "회원가입 유저 등록 API", description = "UserEnrollDto를 토대로 유저를 등록한다.")
     @PostMapping("/user/enroll")
-    public void enrollUser(@RequestBody UserEnrollDto user) {
+    public void enrollUser(
+            @Parameter(description = "사용자 정보 객체", required = true)
+            @RequestBody UserEnrollDto user) {
         loginService.enrollUser(user);
     }
 }

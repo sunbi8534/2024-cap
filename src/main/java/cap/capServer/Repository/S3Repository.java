403e21f6faf8bae715a4f67fdecede6ee9bbdda4
sunbi8534 <sunbi8534@github.com>
2024-05-31 14:51:11 +0900

    url 수정

--- src/main/java/cap/capServer/Repository/S3Repository.java
+++ src/main/java/cap/capServer/Repository/S3Repository.java
@@ -1,11 +1,16 @@
 package cap.capServer.Repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
@@ -28,8 +33,15 @@ public class S3Repository {
     }
 
     public void saveGeneratedUrl(int id, String url) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> result = new HashMap<>();
+        try {
+            result = objectMapper.readValue(url, Map.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
         String updateSql = "update url set url = ? and progress = true where id = ?;";
-        jdbcTemplate.update(updateSql, url, id);
+        jdbcTemplate.update(updateSql, url, result.get("url"));
     }
 
     public void saveTags(int id, List<String> tags) {

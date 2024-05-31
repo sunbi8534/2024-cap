b7e1549c151bc100a3d5312329e03ec5265d0bb3
sunbi8534 <sunbi8534@github.com>
2024-05-31 15:02:13 +0900

    error 수정

--- src/main/java/cap/capServer/Repository/S3Repository.java
+++ src/main/java/cap/capServer/Repository/S3Repository.java
@@ -40,6 +40,8 @@ public class S3Repository {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
+        System.out.println(result.get("url"));
+        System.out.println(result.get("url").getClass());
         String updateSql = "update url set url = ?, progress = true where id = ?;";
         jdbcTemplate.update(updateSql, url, result.get("url"));
     }
